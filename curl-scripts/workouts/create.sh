36da010b71cc525bdccff711210e139aca4cfe60
kristamcmanus <k.mcmanus310@gmail.com>
2021-04-21 15:56:45 -0400

    Add workouts folder that includes CRUD files

---
+++ curl-scripts/workouts/create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/workouts"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "workout": {
+      "name": "'"${NAME}"'",
+      "description": "'"${DESCRIPTION}"'",
+      "duration": "'"${DURATION}"'",
+      "date": "'"${DATE}"'",
+      "owner": "'"${OWNER}"'"
+    }
+  }'
+
+echo
